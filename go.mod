980c33c585c36b84544112e9eafe2a62d70cac3d
mohammed <mohammedfuta@gmail.com>
2024-02-03 12:33:26 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mohammedfuta2000/slack-file-bot
+
+go 1.21.3
+
+require github.com/slack-go/slack v0.12.1
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main()  {
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-6425834389143-6580047798898-P1JyZYeHwVGq6ni8LHc7T8Lb")
+	os.Setenv("CHANNEL_ID","C06DB0DJMMX")
+	api:=slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr:=[]string{os.Getenv("CHANNEL_ID")}
+	fileArr:=[]string{"myfile.txt"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params:=slack.FileUploadParameters{
+			Channels: channelArr,
+			File: fileArr[i],
+		}
+		file,err:=api.UploadFile(params)
+		if err!=nil {
+			fmt.Printf("%s\n",err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n",file.Name,file.URLPrivate)
+	}
+}
